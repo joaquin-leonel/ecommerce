a0dbb535b10a0c883410df15687e3e9613492723
joaquin leonel <woriltomo10@gmail.com>
2021-11-17 15:58:14 -0300

    primera entrega

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Entrega clase 2 probando React JS
+        </p>
+        <a
+          className="App-link"
+          href="https://coderhouse.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coder House
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
